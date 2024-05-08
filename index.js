827ea5709ee3d51f5c2635cccb2eae511d794dfb
guzegui <guzmanegui@gmail.com>
2024-05-08 12:49:36 +0200

    All tests pass

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 class SortedList {
   constructor() {
-    this.items = []
-    this.length = 0
+    this.items = [];
+    this.length = 0;
   }
 
   add(item) {
-    // i removed the if condition as we do  this.items.push(item) we are already putting one element only 
+    // i removed the if condition as we do  this.items.push(item) we are already putting one element only
     this.items.push(item);
     this.length++;
 
     this.items.sort((a, b) => a - b);
   }
 
-
   get(pos) {
     if (pos > this.length || pos < 0) throw new Error("OutOfBounds");
     else {
       //  (ps : no need for getting index as index is alreay the position passed to the fnction )
-      return this.items[pos]
+      return this.items[pos];
     }
   }
   // mde use of the sorted array way to get the last element (that is the biggest)
   max() {
     if (this.length == 0) throw new Error("EmptySortedList");
     else {
-      const sorted = this.items.sort((a, b) => a - b);
-      return sorted[sorted.length - 1]
-        ;
-
-
+      //const sorted = this.items.sort((a, b) => a - b);
+      return this.items[this.length - 1];
     }
   }
 
+  min() {
+    if (this.length == 0) throw new Error("EmptySortedList");
+    return this.get(0); // minimum value is always the first element
+  }
 
-  min() { }
+  sum() {
+    if (this.length == 0) return 0;
+    return this.items.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0);
+  }
 
-  sum() { }
-
-  avg() { }
+  avg() {
+    if (this.length == 0) throw new Error("EmptySortedList");
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
